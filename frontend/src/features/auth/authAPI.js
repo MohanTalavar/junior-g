8b2c7ec5d4f02d502d37fcf7356985b83448da8b
MohanTalavar <mohan.talawar.20@gmail.com>
2025-04-26 17:25:09 +0530

    Removing old FE

--- frontend/src/features/auth/authAPI.js
+++
@@ -1,6 +0,0 @@
-import api from "../../api/Junior_G_Apis";
-
-export const loginUser = async (credentials) =>{
-    const response =  await api.post("/users/login",credentials);
-    return response.data;
-};
