8170f8f94066a9893e0d3fe90cc9b43efa6c2426
MohanTalavar <mohan.talawar.20@gmail.com>
2025-10-26 21:31:05 +0530

    Changes in GlobalCorsConfig, restricted access

--- backend/src/main/java/com/juniorg/config/GlobalCorsConfig.java
+++ backend/src/main/java/com/juniorg/config/GlobalCorsConfig.java
@@ -8,9 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class GlobalCorsConfig {
 
+/*  Commenting this out so we have have more security updated on 26 Oct 25
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // allow all endpoints
@@ -22,6 +26,25 @@ public class GlobalCorsConfig {
                         .allowedOriginPatterns("*") // use this instead of allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }*/
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://juniorg.site",
+                                "http://juniorgpreschool.s3-website.ap-south-1.amazonaws.com",
+                                "https://juniorgpreschool.s3-website.ap-south-1.amazonaws.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Content-Type", "Authorization")
                         .allowCredentials(true);
             }
         };
